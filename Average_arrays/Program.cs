169b87157237aa0525105df49f04e783be6389bb
Ana Pavlovic <49585841+pavlovicana@users.noreply.github.com>
2019-04-16 11:16:47 +0200

    initial commit

---
+++ Average_arrays/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Average_arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = new int[6] { 7, 12, 22, 77, 2, 6 };
+            int[] arr2 = new int[6] { 7, 12, 22, 77, 2, 6 };
+            Average(arr1, arr2);
+            Console.ReadLine();        
+
+        }
+        public static void Average(int[]arr1, int[]arr2)
+        {
+            int av1 = 0;
+            int av2 = 0;
+            int sum1 = 0;
+            int sum2 = 0;
+
+            for(int i = 0; i < arr1.Length; i++)
+            {
+                sum1 += arr1[i];
+            }
+            av1 = sum1 / arr1.Length;
+            Console.WriteLine("Average of the first array is: {0}", av1);
+
+            for (int i = 0; i < arr2.Length; i++)
+            {
+                sum2 += arr2[i];
+            }
+            av2 = sum2 / arr2.Length;
+            Console.WriteLine("Average of the second array is: {0}", av2);
+
+        }
+    }
+}
---
+++ Average_arrays/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Average_arrays")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Average_arrays")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("70fc252f-5a24-4af8-b290-684597cfa5be")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
