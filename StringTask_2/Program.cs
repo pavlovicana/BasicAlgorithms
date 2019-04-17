d74b7d6ee9a6769c9d452dde249b4823031aadd9
Ana Pavlovic <49585841+pavlovicana@users.noreply.github.com>
2019-04-17 12:09:13 +0200

    initial commit

---
+++ StringTask_2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringTask_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "Today is a beautiful day.";
+            Console.WriteLine("Count number of words in string.");
+            int count = 0;
+
+            for(int i = 0; i < s1.Length; i++)
+            {
+                if(s1[i] == ' ')
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine(count);
+            Console.ReadLine();
+
+        }
+    }
+}
