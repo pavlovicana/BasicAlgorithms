8ef4f29b9cfc4d60a9a31a23ed2bc9896bfd3cec
Ana Pavlovic <49585841+pavlovicana@users.noreply.github.com>
2019-04-17 11:20:38 +0200

    initial commit

---
+++ StringChallenge/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string unetiTekst = "Danas je divan dan.";
+            Console.WriteLine("Unesi lokaciju cara koji zelis da ti se ispise na konzoli.");
+            int lokacija = Convert.ToInt32(Console.ReadLine());
+
+            try
+            {
+                Console.WriteLine(unetiTekst[lokacija - 1]);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Lokacija ne odgovara duzini naseg tekta." + e);
+            }
+            Console.ReadLine();
+        }
+
+
+    }
+
+}
