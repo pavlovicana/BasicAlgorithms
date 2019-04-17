6ce3006befcffbdb6d9e7cb78f9c048756ed243a
Ana Pavlovic <49585841+pavlovicana@users.noreply.github.com>
2019-04-17 11:40:17 +0200

    initial commit

---
+++ StringJoin/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringJoin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "This is a string to be copied.";
+            string[] s2 = new string[s1.Length];
+
+            int i = 0;
+            int count = 0;
+
+            for(i = 0; i < s1.Length; i++)
+            {
+                string temp = s1[i].ToString();
+                s2[i] = temp;
+                count++;
+            }
+            Console.WriteLine(s1);
+            Console.WriteLine(string.Join("", s2));
+            Console.WriteLine(count);
+            Console.ReadLine();
+
+        }
+    }
+}
