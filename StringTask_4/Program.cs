0338ad459178d5ad6fab77b2211d8e1768890b50
Ana Pavlovic <49585841+pavlovicana@users.noreply.github.com>
2019-04-17 12:31:50 +0200

    initial commit

---
+++ StringTask_4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringTask_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string original = "this is a string";
+            string test = "test";
+
+            char ch1 = original.ElementAt(8);
+            string[] split = original.Split(ch1);
+            string s1 = " ";
+            s1 += split[0] + test + " " + ch1 + split[1];
+            Console.WriteLine(s1);
+            Console.ReadLine();
+
+        }
+    }
+}
