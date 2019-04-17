09dd8aaaf8d59b8b954bbf914cafc316d9e37181
Ana Pavlovic <49585841+pavlovicana@users.noreply.github.com>
2019-04-17 12:23:34 +0200

    initial commit

---
+++ StringTask_3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringTask_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "Welcome to w3resource.com ";
+            int letter = 0;
+            int otherchars = 0;
+            int numbers = 0;
+
+            for (int i = 0; i < s1.Length; i++)
+            {
+                if ((s1[i] >= 'a' && s1[i] <= 'z') || (s1[i] >= 'A' && s1[i] <= 'Z'))
+                {
+                    letter++;
+                }
+                else if (s1[i] >= '0' && s1[i] <= '9')
+                {
+                    numbers++;
+                }
+                else
+                {
+                    otherchars++;
+                }
+            }
+            Console.WriteLine(letter);
+            Console.WriteLine(numbers);
+            Console.WriteLine(otherchars);
+            Console.ReadLine();
+
+        }
+    }
+}
