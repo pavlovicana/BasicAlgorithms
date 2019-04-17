730dbd454dd63a64bf2b33a38e42a80fa717f737
Ana Pavlovic <49585841+pavlovicana@users.noreply.github.com>
2019-04-17 11:10:12 +0200

    initial commit

---
+++ Factorial/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int result = Factorial(num);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+        public static int Factorial(int number)
+        {
+            int fac = 1;
+            while (number > 0)
+            {
+                fac = number * fac;
+                number--;
+            }
+            return fac;
+        }
+    }
+}
