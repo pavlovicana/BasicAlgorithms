15873b7192b3a520cf8c413155b3a5e42808e93d
Ana Pavlovic <49585841+pavlovicana@users.noreply.github.com>
2019-05-16 12:41:02 +0200

    Add files via upload
    initial commit

---
+++ SwapNumbers/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwapNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //swap two integers
+            int a = 12;
+            int b = 32;
+            int temp = 0;
+
+            temp = b;
+            b = a;
+            a = temp;
+
+            Console.WriteLine("a = " + a);
+            Console.WriteLine("b = " + b);
+
+            //without using temp variable
+
+            int x = 2;
+            int y = 8;
+
+            x = x + y;
+            y = x - y;
+            x = x -y;
+
+            Console.WriteLine("x = " + x);
+            Console.WriteLine("y = " + y);
+            Console.ReadLine();
+
+
+
+
+
+
+        }
+    }
+}
